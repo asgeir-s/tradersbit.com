958142374cfbcff2a2d6304dc12c930c63e7a265
cluda <asgeir@cluda.no>
2016-01-19 16:37:03 +0100

    new design + how to

--- src/app/index.config.ts
+++ src/app/index.config.ts
@@ -5,9 +5,23 @@ export function config($logProvider: angular.ILogProvider, $locationProvider: an
   
   $logProvider.debugEnabled(true);
 
+      // Extend the red theme with a few different colors
+  var customCyan = $mdThemingProvider.extendPalette('cyan', {
+    '500': '3893C6',
+    'contrastDefaultColor': 'light'
+  });
+    var customOrange = $mdThemingProvider.extendPalette('orange', {
+    '500': 'FEE496',
+    'A200': 'FEE496',
+    'contrastDefaultColor': 'dark'
+  });
+  // Register the new color palette map with the name <code>neonRed</code>
+  $mdThemingProvider.definePalette('customCyan', customCyan);
+  $mdThemingProvider.definePalette('customOrange', customOrange);
+  // Use that theme for the primary intentions
   $mdThemingProvider.theme('default')
-    .primaryPalette('cyan')
-    .accentPalette('orange');
+    .primaryPalette('customCyan')
+    .accentPalette('customOrange');
   
    authProvider.init({
     domain: 'cluda.auth0.com',
